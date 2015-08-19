cc83d2a3d9462aaf81ee227825ba243a32b6cb1e
vlg <gorbenko.vova@gmail.com>
2015-08-19 16:57:13 +0300

    refactor

--- Lib/Extensions/NSURL+Cookies.swift
+++ Lib/Extensions/NSURL+Cookies.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 public extension NSURL {
-
+    
     func logCookies() {
         
         var cookiesLog = "Cookies for url: \(self)\n"
-    
+        
         if let cookies = NSHTTPCookieStorage.sharedHTTPCookieStorage().cookiesForURL(self) as? [NSHTTPCookie] {
             
             for cookie in cookies {
@@ -21,7 +21,7 @@ public extension NSURL {
                 cookiesLog += "Name: '\(cookie.name)'; Value: '\(cookie.value)'\n"
             }
         }
-    
+        
         NSLog(cookiesLog)
     }
     
@@ -32,7 +32,6 @@ public extension NSURL {
         
         if let cookies = cookies {
             for cookie in cookies {
-            
                 cookieStorage.deleteCookie(cookie as NSHTTPCookie)
             }
         }
